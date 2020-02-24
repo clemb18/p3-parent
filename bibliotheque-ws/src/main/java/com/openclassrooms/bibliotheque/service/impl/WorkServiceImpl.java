16488f4cfc3bc83d6c8f4c955467757f94b3d7dc
clemb18 <clem.biotteau@gmail.com>
2020-02-24 09:58:25 +0100

    Update WorkServiceImpl.java

--- bibliotheque-ws/src/main/java/com/openclassrooms/bibliotheque/service/impl/WorkServiceImpl.java
+++ bibliotheque-ws/src/main/java/com/openclassrooms/bibliotheque/service/impl/WorkServiceImpl.java
@@ -9,11 +9,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 
 @Service
+@Transactional
 public class WorkServiceImpl implements WorkService {
 
     private static Logger logger = LoggerFactory.getLogger(WorkServiceImpl.class);
